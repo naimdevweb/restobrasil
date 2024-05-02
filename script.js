bf30dcc95e65c21da7bc77cab13c1f52237d92a9
naimdevweb <166521934+naimdevweb@users.noreply.github.com>
2024-05-02 09:13:01 +0200

    brasilsite
    modification site

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ fetch("restaurantBresilien.json")
   // Affiche les données dans la console du navigateur
   let plat = "";
     data.entreprise.plats.forEach((p) => {
-      plat += `<section class="flexplat" 
+      plat += `<section class="flexplat" >
 
       
       
       
       
-<div class="texteplat" data-aos="zoom-out-right">
+<div class="texteplat" >
 
 
       
-      <div class="flextout" >  <h3 >${p.nom}</h3> <div class="descrip"> <p> ${p.description}</p>  </div> <div class="imgdes">  <img class="flexlimage" src="${p.image}"/> </div>  </div>  </div>  </section>
+      <div class="flextout">  <h3 >${p.nom}</h3> <div class="descrip"> <p> ${p.description}</p>  </div>  <img class="flexlimage"  src="${p.image}"/> </div>  </div>    </section>
 
       `;
     });
@@ -48,20 +48,31 @@ fetch("restaurantBresilien.json")
 
       document.querySelector(".content").innerHTML +=
       `
-      <div class="hero"  > <p class="texte" data-aos="flip-down" > ${data.entreprise.nomCommercial} ${data.entreprise.avantagesClients} ${data.entreprise.texteAppelAction}</p> </div>
+      <div class="hero"> <p class="texte"  > ${data.entreprise.nomCommercial} ${data.entreprise.avantagesClients} ${data.entreprise.texteAppelAction}</p> </div>
       
-      <div class="naim">  <p class="texte" data-aos="flip-down">   ${data.entreprise.phraseAccroche} </p>
+      <div class="naim">  <p class="texte" >  ${data.entreprise.phraseAccroche} </p>
 
       </div>   
       
       <strong class="decouvre"><h2>Decouvrez nos specialités:</h2></strong>
-        ${plat} <h1> Les avis:</h1> 
+        ${plat} <h1> Les avis:</h1> <div class="infoservice">${service}</div>
        
 
       
        
-       <div class="serv"> <div class="service" >${service}  ${temoignage} </div>   </div>  `
+       <div class="serv"> <div class="service">   ${temoignage} </div> </div>  `
       
   }
+
+
+  var map = L.map('map').setView([43.284121, 5.564604], 13);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'jpg'
+}).addTo(map);
+L.marker([43.284121,5.564604]).addTo(map)
+    .bindPopup("<h3> Restaurant Tudo De Bom</h3><p>Impasse de l'Agrie, 13400 Aubagne, Marseille</p>")
+    
   
 
